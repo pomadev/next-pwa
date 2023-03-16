aec0eae0fc4c63d7f19886759c0f38f1a8db2a4d
pomadev <y.pomadev@gmail.com>
2023-03-17 03:58:19 +0900

    ユーザー情報編集実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             theme='dark'
           />
         ) : (
-          <Account />
+          <Account session={session} />
         )}
       </div>
     </>
