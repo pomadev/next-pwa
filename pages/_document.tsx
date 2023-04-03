f3410e4c98704eae77b80fa5f9cdc36e80ec82c6
pomadev <y.pomadev@gmail.com>
2023-04-03 14:47:13 +0900

    web-pushテスト

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,6 +9,15 @@ export default function Document() {
         <link rel='apple-touch-icon' href='/icon.png' />
         <link rel='icon' href='/favicon.ico' />
         <meta name='theme-color' content='#101010' />
+        <Script src='https://cdn.onesignal.com/sdks/OneSignalSDK.js' defer />
+        <Script id='one-signal-web-push' strategy='afterInteractive'>
+          {`window.OneSignal = window.OneSignal || [];
+            OneSignal.push(function() {
+              OneSignal.init({
+                appId: "c0cadcc9-1e9f-4700-a909-f6a9c24640fa",
+              });
+            });`}
+        </Script>
       </Head>
       <body>
         <Main />
