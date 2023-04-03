66ebcb003404e589a8981636b897bd2647ea9462
pomadev <y.pomadev@gmail.com>
2023-04-03 16:43:22 +0900

    Revert "react-onesignal test"
    This reverts commit 76a5b5e204c0c955841acbd1c6d3cf7606a18093.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import OneSignal from 'react-onesignal'
 
 export default function App({
   Component,
@@ -12,15 +11,31 @@ export default function App({
   const [supabase] = useState(() => createBrowserSupabaseClient())
 
   useEffect(() => {
-    OneSignal.init({
-      appId: 'c0cadcc9-1e9f-4700-a909-f6a9c24640fa',
-      safari_web_id: 'web.onesignal.auto.613528e9-2930-4b07-a098-5a9518822d98',
-      // allowLocalhostAsSecureOrigin: process.env.NODE_ENV === 'development',
-      serviceWorkerParam: {
-        scope: '/js/push/onesignal/',
-      },
-      serviceWorkerPath: './js/push/onesignal/OneSignalSDKWorker.js',
+    // @ts-ignore
+    window.OneSignal = window.OneSignal || []
+    // @ts-ignore
+    OneSignal.push(function () {
+      // @ts-ignore
+      OneSignal.init({
+        appId: 'c0cadcc9-1e9f-4700-a909-f6a9c24640fa',
+        safari_web_id:
+          'web.onesignal.auto.613528e9-2930-4b07-a098-5a9518822d98',
+        notifyButton: {
+          enable: true,
+        },
+        serviceWorkerParam: {
+          scope: '/js/push/onesignal/',
+        },
+        serviceWorkerPath: './js/push/onesignal/OneSignalSDKWorker.js',
+        serviceWorkerUpdaterPath:
+          '/js/push/onesignal/OneSignalSDKUpdaterWorker.js',
+      })
     })
+
+    return () => {
+      // @ts-ignore
+      window.OneSignal = undefined
+    }
   }, [])
 
   return (
