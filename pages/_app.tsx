2f9053f88f0ee752fe3c4700f96f6b65a32137ce
pomadev <y.pomadev@gmail.com>
2023-04-04 18:38:40 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
@@ -9,6 +9,33 @@ export default function App({
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  useEffect(() => {
+    // @ts-ignore
+    window.OneSignal = window.OneSignal || []
+    // @ts-ignore
+    OneSignal.push(function () {
+      // @ts-ignore
+      OneSignal.init({
+        appId: '9d61fe6c-9cbc-4649-855f-128f6cafe7ed',
+        serviceWorkerParam: {
+          scope: '/js/push/onesignal/',
+        },
+        serviceWorkerPath: './js/push/onesignal/OneSignalSDKWorker.js',
+        serviceWorkerUpdaterPath:
+          '/js/push/onesignal/OneSignalSDKUpdaterWorker.js',
+        allowLocalhostAsSecureOrigin: true,
+      })
+      // @ts-ignore
+      OneSignal.showSlidedownPrompt()
+    })
+    // @ts-ignore
+
+    return () => {
+      // @ts-ignore
+      window.OneSignal = undefined
+    }
+  }, [])
 
   return (
     <SessionContextProvider
